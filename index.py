2a127d32b9bbd2766057cb56b575706e34c4603a
Dmitry Yakhnov <dmitry@yakhnov.info>
2020-11-23 13:37:03 +1100

    Use memcached to store current DNS state

--- index.py
+++ index.py
@@ -1,13 +1,30 @@
 import sys
 import os
 import yaml
+import json
 import requests
 from time import sleep
+
+from pymemcache.client.base import Client
 
 from designateclient.v2 import client
 
 from keystoneauth1.identity import generic
 from keystoneauth1 import session as keystone_session
+
+
+def json_serializer(key, value):
+    if type(value) == str:
+        return value, 1
+    return json.dumps(value), 2
+
+
+def json_deserializer(key, value, flags):
+    if flags == 1:
+        return value
+    if flags == 2:
+        return json.loads(value)
+    raise Exception("Unknown serialization format")
 
 
 print("Reading settings...")
@@ -22,26 +39,38 @@ if settings["sleep"] != 0:
     print("\nSleeping...")  # allow hosts to save latency information
     sleep(settings["sleep"])
 
-auth = generic.Password(
-    auth_url=settings["openstack"]["endpoint"],
-    username=settings["openstack"]["username"],
-    password=settings["openstack"]["password"],
-    project_name=settings["openstack"]["project"]["name"],
-    project_domain_id="default",
-    user_domain_id="default")
-session = keystone_session.Session(auth=auth)
-client = client.Client(session=session)
-
 try:
-    print("\nGetting current DNS records...")
-    records = client.recordsets.get(
-        settings["dns"]["zone"],
-        settings["dns"]["name"])
-    for ip in records["records"]:
-        print("- {}".format(ip))
+    mc = Client(
+        (settings["memcached"]["server"], settings["memcached"]["port"]),
+        serializer=json_serializer,
+        deserializer=json_deserializer)
 except Exception as e:
     print(str(e))
-    sys.exit("Can't fetch DNS information.")
+    sys.exit("Can't connect to Memcached.")
+
+try:
+    auth = generic.Password(
+        auth_url=settings["openstack"]["endpoint"],
+        username=settings["openstack"]["username"],
+        password=settings["openstack"]["password"],
+        project_name=settings["openstack"]["project"]["name"],
+        project_domain_id="default",
+        user_domain_id="default")
+    session = keystone_session.Session(auth=auth)
+    client = client.Client(session=session)
+except Exception as e:
+    print(str(e))
+    sys.exit("Can't connect to Nectar.")
+
+token = "dns-lb"
+
+print("\nGetting current DNS records...")
+records = mc.get(token)
+if records is None:
+    records = []
+for ip in records:
+    print("- {}".format(ip))
+
 
 hosts = {}
 print("\nGetting latency...")
@@ -62,15 +91,15 @@ if len(hosts) == 0:
     print("\nNo update due to no hosts information available.")
 else:
     dns = []
-    for ip in records["records"]:
+    for ip in records:
         if ip in hosts:
             dns.append(ip)
     for ip in hosts:
         if ip not in dns:
             dns.append(ip)
-    if len(dns) > 1:
+    if len(records) != 0 and len(dns) > 1:
         dns = dns[1:] + dns[:1]
-    if dns != records["records"]:
+    if dns != records:
         print("\nUpdating DNS records...")
         for ip in dns:
             print("- {}".format(ip))
@@ -83,6 +112,7 @@ else:
                     "records": dns
                 }
             )
+            mc.set(token, dns)
         except Exception as e:
             print(str(e))
             sys.exit("Can't update DNS information.")
